dfad2f90bff7025363ca375dba8d7eee6b26ae98
unknown <hautersolomon@gmail.com>
2024-02-23 15:52:21 -0600

    a

--- utils/auth.js
+++ utils/auth.js
@@ -1,5 +1,19 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import { clientCredentials } from './client';
+
+const registerUser = (id) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/user`, {
+    method: 'POST',
+    body: JSON.stringify(id),
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+  })
+    .then((resp) => resolve(resp.json()))
+    .catch(reject);
+});
 
 const signIn = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
@@ -10,4 +24,4 @@ const signOut = () => {
   firebase.auth().signOut();
 };
 
-export { signIn, signOut };
+export { signIn, signOut, registerUser };
